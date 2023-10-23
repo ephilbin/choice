bc3908365d8ba9fa071481ed28f40a1b71879955
ephilbin <143443264+ephilbin@users.noreply.github.com>
2023-10-23 14:56:59 -0400

    Add files via upload

---
+++ choice.py
@@ -0,0 +1,6 @@
+import random
+coin = random.choice(["heads", "tails"])
+#square brackets tell you we're making a list
+#parenthesis are partof the choice function
+#pass an argument in the choice function
+print(coin)
